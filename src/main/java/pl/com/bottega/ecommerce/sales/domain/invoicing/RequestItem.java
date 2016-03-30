34dacc5fec164dda5cdbd7f2e91c5b875618c399
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-03-30 14:55:47 +0200

    Wdrozenie strategii IssuanceStrategy

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestItem.java
@@ -1,24 +1,12 @@
-/*
- * Copyright 2011-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import java.math.BigDecimal;
+
 class RequestItem {
+	private IssuanceStrategy issuanceStrategy;
 
 	private ProductData productData;
 
@@ -30,6 +18,21 @@ class RequestItem {
 		this.productData = productData;
 		this.quantity = quantity;
 		this.totalCost = totalCost;
+
+		switch (productData.getType()) {
+			case DRUG:
+				issuanceStrategy = new DrugIssuanceStrategy();
+				break;
+			case FOOD:
+				issuanceStrategy = new FoodIssuanceStrategy();
+				break;
+			case STANDARD:
+				issuanceStrategy = new StandardIssuanceStrategy();
+				break;
+
+			default:
+				throw new IllegalArgumentException(productData.getType() + " not handled");
+		}
 	}
 
 	public Money getTotalCost() {
@@ -44,4 +47,7 @@ class RequestItem {
 		return quantity;
 	}
 
+	public IssuanceStrategy getIssuanceStrategy() {
+		return issuanceStrategy;
+	};
 }
